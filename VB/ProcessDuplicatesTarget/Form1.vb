cba761041e8cf5b9516cd643709e444b37cfe045
DevExpressExampleBot <fakeEmail>
2019-02-27 16:46:01 +0300

    Source auto update [skip ci]

---
+++ VB/ProcessDuplicatesTarget/Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports DevExpress.XtraReports.UI
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+
+Namespace ProcessDuplicatesTarget
+	Partial Public Class Form1
+		Inherits Form
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Dim report As New ProductsReport()
+			report.NoMerge()
+		End Sub
+
+		Private Sub button2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button2.Click
+			Dim report As New ProductsReport()
+			report.MergeByTag()
+		End Sub
+
+		Private Sub button3_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button3.Click
+			Dim report As New ProductsReport()
+			report.MergeByValue()
+		End Sub
+	End Class
+End Namespace
